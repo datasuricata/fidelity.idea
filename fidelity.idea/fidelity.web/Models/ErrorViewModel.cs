8612d12ded37b6732f739d07456277d9ebd81cac
Lucas Rocha <at.lucasrocha@outlook.com>
2019-03-18 18:00:03 -0300

    FirstCommit

---
+++ fidelity.idea/fidelity.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fidelity.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
